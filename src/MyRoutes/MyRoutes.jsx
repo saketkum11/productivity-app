67976de271be84e80f0d87c77bc83981919dda79
Saket kumar <kumarsaket601@gmail.com>
2023-04-16 17:58:22 +0530

    created:pages

---
+++ src/MyRoutes/MyRoutes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes ,Route} from "react-router-dom";
+
+const MyRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route index path={}></Route>
+        <Route  path={}></Route>
+        <Route index path={}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export { MyRoutes };
---
+++ src/pages/server.js
@@ -0,0 +1,5 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { Timer } from "./Timer";
+import { Task } from "./Task";
+import { Setting } from "./Setting";
